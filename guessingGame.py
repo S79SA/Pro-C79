f830a36f6f65c950b201514d13abfa80e3500baf
S79SA <88217640+S79SA@users.noreply.github.com>
2021-07-30 21:16:18 -0400

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,10 @@
+guess=input("enter your number: ")
+minimumNumber=1
+maxNumber=10
+for g in guess:
+    minimumNumber=minimumNumber+1
+    if(g==' '):
+        maxNumber=maxNumber+1
+print (minimumNumber)
+print(maxNumber)
+
